90ff4301f6f7e76e01bd1db67c280bb36329a5bf
Samarth Parmeshwar Ikkalaki <samarthikkalaki123@gmail.com>
2023-06-26 11:20:18 +0530

    Add Product is Completely Done

--- admin/src/features/product/productSlice.js
+++ admin/src/features/product/productSlice.js
@@ -1,57 +1,82 @@
 import { createAsyncThunk, createSlice, createAction } from "@reduxjs/toolkit";
-import productService from './productService';
-
+import productService from "./productService";
 
 const initialState = {
-    products : [],
-    isLoading : false,
-    isError : false,
-    isSuccess : false,
-    res : {}
-}
+  products: [],
+  isLoading: false,
+  isError: false,
+  isSuccess: false,
+  res: {},
+  message:""
+};
 
-export const getAllProducts = createAsyncThunk("product/all-products",async(thunkAPI) => {
+export const getAllProducts = createAsyncThunk(
+  "product/all-products",
+  async (thunkAPI) => {
     try {
-        return await productService.getAllProducts();
+      return await productService.getAllProducts();
     } catch (error) {
-        return thunkAPI.rejectWithValue(error);
+      return thunkAPI.rejectWithValue(error);
     }
-})
+  }
+);
 
 export const resetState = createAction("Reset_all");
 
-export const createAProduct = createAsyncThunk("product/create",async(data,
-    thunkAPI) =>{
+export const createAProduct = createAsyncThunk(
+  "product/create",
+  async (data, thunkAPI) => {
     try {
-        return await productService.createAProduct(data);
+      return await productService.createAProduct(data);
     } catch (error) {
-        return thunkAPI.rejectWithValue(error);
+      return thunkAPI.rejectWithValue(error);
     }
-})
+  }
+);
 
 const productSlice = createSlice({
-    name : "product",
-    initialState,
-    reducers:{},
-    extraReducers : (builder) => {
-        builder.addCase(getAllProducts.pending,(state,action)=>{
-            state.isLoading = true;
-        })
-        builder.addCase(getAllProducts.fulfilled ,(state, action)=> {
-            state.isLoading = false;
-            state.isSuccess = true;
-            state.isError = false;
-            state.products = action.payload.products;
-            state.res = action.payload.res;
-        })
-        builder.addCase(getAllProducts.rejected, (state, action) => {
-          state.isLoading = false;
-          state.isSuccess = false;
-          state.isError = true;
-          state.products = null;
-          state.res = null;
-        });
-    }
-})
+  name: "product",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getAllProducts.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(getAllProducts.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+      state.isError = false;
+      state.products = action.payload.products;
+      state.res = action.payload.res;
+    });
+    builder.addCase(getAllProducts.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.isError = true;
+      state.products = null;
+      state.message = action.error;
+      state.res = null;
+    });
+
+    builder.addCase(createAProduct.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(createAProduct.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = true;
+      state.createdProduct = action.payload;
+      state.res = action.payload.res;
+    });
+    builder.addCase(createAProduct.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.isSuccess = false;
+      state.message = action.error;
+      state.res = null;
+    });
+    builder.addCase(resetState, () => initialState);
+  },
+});
 
 export const productReducer = productSlice.reducer;
